4dfc75097ba85bee50a514bc87d65555c98c4b94
gadelha <vicenzoggreco@gmail.com>
2024-03-18 09:11:39 -0300

    criçao do if e do input

---
+++ aula01/If.java
@@ -0,0 +1,16 @@
+package aula01;
+
+public class If {
+    public static void main(String[] args) {
+        int x = 17;
+        int y = 18;
+
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else if (x == y) {
+            System.out.println("x e y sao iguais");
+        } else {
+            System.out.println("Y é maior que x");
+        }
+    }
+}
---
+++ aula01/Input.java
@@ -0,0 +1,15 @@
+package aula01;
+
+import java.util.Scanner;
+
+class Input{
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        String msg;
+
+        System.out.println("Digite sua Mensagem");
+        msg = myObj.nextLine();
+
+        System.out.println(msg);
+    }
+}
